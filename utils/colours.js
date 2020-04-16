3196d027897167e79df64c15ba0240cd7b74010b
Pádraig Howlin <padraig@howlin.ie>
2020-04-16 16:34:48 +0100

    feat: our colours

---
+++ utils/colours.js
@@ -0,0 +1,8 @@
+export const green = '#8FD14F'
+export const blue = '#2D9BF0'
+export const purple = '#652CB3'
+export const pink = '#DA0063'
+export const orange = '#FAC710'
+export const grey = '#808080'
+export const lightGrey = '#E6E6E6'
+export const white = '#ffffff'
